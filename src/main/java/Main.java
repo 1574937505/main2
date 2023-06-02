5af585cbc70743b2793657d28efebbdd7e6a79c0
LiuLang <1574937505@qq.com>
2023-06-02 21:53:21 +0800

    [6.2]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello World!");
+        int spoon7 = 7;
+        int spoon11 = 11;
+        int target = 2;
+
+        int[] result = findSpoonCounts(spoon7, spoon11, target);
+
+        if (result[0] == -1) {
+            System.out.println("无法量出目标酱油的重量。");
+        } else {
+            System.out.println("舀取" + result[0] + "次" + spoon7 + "两勺子，舀取" + result[1] + "次" + spoon11 + "两勺子。");
+        }
+    }
+
+    public static int[] findSpoonCounts(int spoon7, int spoon11, int target) {
+        int[] result = {-1, -1};
+
+        for (int count11 = 0; count11 <= target / spoon11; count11++) {
+            int remaining = target - count11 * spoon11;
+            if (remaining % spoon7 == 0) {
+                int count7 = remaining / spoon7;
+                result[0] = count7;
+                result[1] = count11;
+                break;
+            }
+        }
+
+        return result;
     }
 }
